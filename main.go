61d6a85e301a5e3b7521e3dfa01d4b2695803658
jeongsikim <jeongsikim@ebay.com>
2021-09-11 12:43:36 +0900

    Our First Blockchain

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"github.com/drinkwale/nomad-coin/person"
 )
 
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev Hash: %s\n\n", block.prevHash)
+	}
+}
+
 func main() {
-	jack := person.Person{}
-	jack.SetDetails("jack", 34)
-	fmt.Println("Main's jack : ", jack)
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
 }
